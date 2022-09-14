922a0339b82449f61d17400d47b4dd33deed4538
yumi <sunny8bliss@gmail.com>
2022-09-14 14:06:11 +0900

    Creating Blog Framing

---
+++ pages/photos.js
@@ -0,0 +1,12 @@
+import HeadInfo from "../components/HeadInfo";
+
+const photos = () => {
+  return (
+    <div>
+      <HeadInfo title="My Blog - Photos" />
+      <h1>My photos</h1>
+    </div>
+  );
+};
+
+export default photos;
